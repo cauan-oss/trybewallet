c8166c43880221ae6d77449c9d757d7bf7a902d9
cauan-oss <cauanbarros14@gmail.com>
2023-01-08 22:09:40 +0000

    table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import Table from './components/Table';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={ Login } />
             <Route exact path="/carteira" component={ Wallet } />
+            <Route exact path="/table" component={ Table } />
           </Switch>
         </BrowserRouter>
       </div>
