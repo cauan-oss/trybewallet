cfbb0687a8021d0286679c241e5905992789469e
cauan-oss <cauanbarros14@gmail.com>
2023-01-10 12:59:41 +0000

    excluir

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { actionDelete } from '../redux/actions';
 
 class Table extends Component {
   funcDelete = (id) => {
